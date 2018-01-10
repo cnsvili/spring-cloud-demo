9b50e74fe93577175518db5c3b34214c57ab8b70
cnsvili <cnsvili@mail.com>
2018-01-10 18:06:51 +0800

    config server and client

---
+++ config-customer/src/main/java/com/littlenb/configcustomer/ConfigCustomerApplication.java
@@ -0,0 +1,12 @@
+package com.littlenb.configcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigCustomerApplication.class, args);
+	}
+}
